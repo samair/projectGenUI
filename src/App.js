a364fc5214b3e34a88a3ce6cab2f3e69d4f8890a
samair <tsameerc@gmail.com>
2019-08-11 11:58:54 +0530

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,152 @@
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css'
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { Button, Form, FormGroup, Label, Input, Progress} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+  constructor(props){
+   super(props)
+  this.state = {isLoading: false,  artifactName:"",loading:0,groupName:""}  
+
+  this.handleSubmit = this.handleSubmit.bind(this)
+  this.handleChange = this.handleChange.bind(this)
+  this.handleGroupName = this.handleGroupName.bind(this)
 }
+   handleGroupName (event){
+    this.setState({loading:0    })
+    this.setState({groupName:event.target.value})
 
-export default App;
+    console.log(this.state.groupName)
+   }
+   handleChange (event){
+    this.setState({loading:0    })
+    this.setState({artifactName:event.target.value})
+
+    console.log(this.state.artifactName)
+   }
+    handleSubmit () {
+
+     this.setState({isLoading: true});
+
+   
+    axios.post('http://localhost:8080/generate/setInfo',{ groupName: this.state.groupName,
+      artifactName: this.state.artifactName, javaVersion:9})
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+                loading: 33
+            
+            })
+
+
+        axios({
+             url: 'http://localhost:8080/generate/maven',
+              method: 'GET',
+            responseType: 'blob', // important
+             onDownloadProgress: (progressEvent) => {
+            var percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+            this.setState({
+                loading: percentCompleted
+            
+            })
+                 },
+            })
+        .then((response) => {
+                this.setState({isLoading: false})
+                  const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                const filename = this.state.artifactName.concat(".zip");
+                link.setAttribute('download', filename);
+                 document.body.appendChild(link);
+                 link.click();
+  
+            });
+
+                // fake server request, getting the file url as response
+                /*
+     setTimeout(() => {
+      const response = {
+        file: 'http://localhost:8080/generate/maven',
+      };
+      // server sent the url to the file!
+      // now, let's download:
+      window.location.href = response.file;
+
+      // you could also do:
+      // window.open(response.file);
+    }, 100);*/
+     
+        
+      })
+
+  }
+
+
+  
+  render() {
+    return (
+        <div >
+    
+        <div className="cent">
+        <p> Maven Java Project Generator</p>
+
+      
+        <FormGroup>
+          <Label for="exampleEmail">Group Name</Label>
+          <Input type="text" name="groupName" id="exampleEmail" placeholder="com.example" disabled={this.state.isLoading} value={this.state.groupName} onChange={this.handleGroupName}/>
+        </FormGroup>
+                <FormGroup>
+          <Label for="exampleEmail">Artificat</Label>
+          <Input type="text" name="artiID" id="exampleEmail" placeholder="testApp" disabled={this.state.isLoading} value={this.state.artifactName} onChange={this.handleChange}/>
+   </FormGroup>
+
+        <FormGroup>
+          <Label for="exampleSelect">JavaVersion</Label>
+          <Input type="select" name="select" id="exampleSelect" disabled={this.state.isLoading}>
+            <option>1.8</option>
+            <option>1.11</option>
+            <option>1.12</option>
+
+          </Input>
+        </FormGroup>
+
+        <FormGroup check>
+          <Label check>
+            <Input type="checkbox"  disabled={this.state.isLoading}/>
+            Kafka Producer
+          </Label>
+        </FormGroup>
+                <FormGroup check>
+          <Label check>
+            <Input type="checkbox" disabled={this.state.isLoading}/>
+            Kafka Consumer
+          </Label>
+        </FormGroup>
+                      <FormGroup check>
+          <Label check>
+            <Input type="checkbox" disabled={this.state.isLoading} />
+            Spring Boot Microservice
+          </Label>
+        </FormGroup>
+        <p>
+        </p>
+
+<FormGroup>
+        <Button type="submit" color="success" onClick={this.handleSubmit} disabled={this.state.isLoading}>
+           Generate Project</Button>
+
+              
+
+   </FormGroup>
+
+      <Progress striped value={this.state.loading} animated />
+   
+      </div>
+      </div>
+    );
+  }
+}
